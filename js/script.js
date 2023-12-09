3c02b1c76d692ce932fdfd8ee91cce581c925074
kervines <kervinzero@gmail.com>
2023-12-09 14:40:40 -0300

    update project

--- js/script.js
+++ js/script.js
@@ -1,37 +1,26 @@
-const kick = document.querySelector('.kick');
-const crash = document.querySelector('.crash');
-const tom = document.querySelector('.tom');
-const ride = document.querySelector('.ride');
-const snare = document.querySelector('.snare');
-const sticks = document.querySelector('.sticks');
+const drumItens = document.querySelectorAll('.drum-item div');
 
-kick.addEventListener('click', playKick);
-function playKick() {
-  const play = document.querySelector('.kick-audio');
-  play.play();
+drumItens.forEach((item) => {
+  item.addEventListener('click', playSound);
+});
+
+// drumItens.forEach((item) => {
+//   document.addEventListener('keypress', playKeypress);
+// });
+
+function playSound() {
+  const startSound = document.querySelector(`.${this.className}-audio`);
+  startSound.currentTime = 0;
+  startSound.play();
 }
-tom.addEventListener('click', playTom);
-function playTom() {
-  const play = document.querySelector('.tom-audio');
-  play.play();
-}
-crash.addEventListener('click', playCrash);
-function playCrash() {
-  const play = document.querySelector('.crash-audio');
-  play.play();
-}
-ride.addEventListener('click', playRide);
-function playRide() {
-  const play = document.querySelector('.ride-audio');
-  play.play();
-}
-snare.addEventListener('click', playSnare);
-function playSnare() {
-  const play = document.querySelector('.snare-audio');
-  play.play();
-}
-sticks.addEventListener('click', playSticks);
-function playSticks() {
-  const play = document.querySelector('.sticks-audio');
-  play.play();
-}
+
+// function playKeypress(event) {
+//   console.log(event.key);
+// }
+
+//nome dos itens da bateria
+drumItens.forEach((item) => {
+  const p = document.createElement('p');
+  p.innerText = item.className;
+  item.appendChild(p);
+});
